da67692b5c4dea97c3d0d688926aa88a979c3b55
JesusLN <jelenu@fp.insjoaquimmir.cat>
2023-03-07 15:24:56 +0100

    2 Terminado

--- react/src/places/marks/PlaceMark.jsx
+++ react/src/places/marks/PlaceMark.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-export const PlaceMark = ({mark}) => {
+export const PlaceMark = ({mark, handleDeleteMark}) => {
   return (
     <tr>
       <td>{mark.id}</td>
       <td>{mark.name}</td>
       <td>{mark.description}</td>
       <td><Link to={mark.ruta}></Link></td>
-      <td>{mark.id}</td>
+      <td><button onClick={ (e) => {handleDeleteMark(mark.id)}}>Borrar</button></td>
     </tr>
    
   )
